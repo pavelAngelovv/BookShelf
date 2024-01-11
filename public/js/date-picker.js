da8087dd2b3611e87f1c8cf117c8654cc6e93bc5
pavelcodixis <pangelov@codixis.com>
2024-01-11 15:02:16 +0200

    Move date picker to seperate js file

---
+++ public/js/date-picker.js
@@ -0,0 +1,17 @@
+const initDatepicker = () => {
+    $('.js-datepicker').datepicker({
+      format: 'yyyy-mm-dd'
+    });
+  };
+  
+  // Event listener for datepicker icon
+  const initDatepickerIconListener = () => {
+    $('.input-group-text').on('click', function () {
+      $(this).closest('.input-group').find('.js-datepicker').focus();
+    });
+  };
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    initDatepicker();
+    initDatepickerIconListener();
+  });
